90e1a8d338e4ba6df09be79a872b84d191bcbffe
onii <fabianmann9@gmail.com>
2023-01-24 17:48:30 +0545

    thang

---
+++ src/components/Form.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="wrap">
+    <div class="main">
+        <h1 class="head">
+            contact us
+        </h1>
+        <input type="text" class="inp" name="name" placeholder="Name" v-model="uname">
+        <input type="email" class="inp" name="email" placeholder="Email" v-model="uemail">
+        <input type="tel" class="inp" name="contact" placeholder="Contact No." v-model="unum">
+        <textarea  v-model="umsg" name="description" id="description" cols="30" rows="10" class="inp" placeholder="Type your message here"></textarea>
+        <input @click="addDocument" type="button" class="inp" value="Submit">
+    </div>
+</div>
+
+</template>
+
+<script>
+
+
+export default{
+
+    name: "my-form",
+    data(){
+        return{
+            uname : "",
+            uemail : "",
+            unum : "",
+            umsg : "",
+            db : null
+        }
+    },
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<nav class="navc">
+        <a id="one"> home</a>
+        <a id="two"> about us</a>
+        <a id="three"> contact us</a>
+        <a id="four"> login</a>
+</nav>
+
+</template>
+
+<script>
+export default{
+    name: "my-home",
+}
+</script>
